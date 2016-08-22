8201aae2fb97a79a4b26e4ed958cda55e6e246d8
James Wu <jw3qz@virginia.edu>
2016-08-22 12:16:11 -0400

    Build environment is prepped and ready, capt'n!

---
+++ app/src/main/java/com/bipbipbap/metronomeScheduler.java
@@ -0,0 +1,14 @@
+package com.bipbipbap;
+
+import metronome.Metronome;
+
+/**
+ * Created by James Wu on 7/29/2016.
+ */
+public class metronomeScheduler {
+
+//    Metronome gigglez;
+
+
+
+}
--- app/src/main/java/com/bipbipbap/play.java
+++ app/src/main/java/com/bipbipbap/play.java
@@ -2,30 +2,46 @@ package com.bipbipbap;
 
 import metronome.*;
 import android.app.Activity;
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 
 /**
  * Created by James Wu on 7/10/2016.
  */
 public class play extends Activity{
 
-    //Components: Tap button, trace line, Blow into phone,
     Button tapButton;
-
     Metronome myMetronome;
+    int bpm = 100;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.play);
         Log.v("AWWW YEEEEAAAAHH ", "POOP");
-        myMetronome = new Metronome();
-        //myMetronome.start(155);
 
+        Action act = new Action() {
+            public void run(){
+                System.out.println("IT'S THE SOCIAL-JUSTICE-MOBILE");
+            }
+        };
+        myMetronome = new Metronome(act, 125);
+
+        tapButton = (Button) findViewById(R.id.button);
+        tapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v.getRootView().setBackgroundColor(Color.GREEN);
+                myMetronome.stop();
+            }
+        });
     }
-
 
 
 
